40177be7ff7eb726ff8ca791c9fd1e936eebad2e
Hager Hesham <h.hesham227766@gmail.com>
2024-04-21 17:18:11 +0300

    cart test

---
+++ Classes/CartTest.java
@@ -0,0 +1,42 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CartTest {
+    private Cart cart;
+    private Item item1;
+    private Item item2;
+
+    @BeforeEach
+    
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void addToCart() {
+    }
+
+    @Test
+    void removeFromCart() {
+    }
+
+    @Test
+    void increaseAmount() {
+    }
+
+    @Test
+    void decreaseAmount() {
+    }
+
+    @Test
+    void emptyCart() {
+    }
+
+    @Test
+    void getTotalPrice() {
+    }
+}
