848b9f74a6d5306fa4f72ff19fc8f9908dfe53d1
Mariam Abdou <21p0214@eng.asu.edu.eg>
2024-04-21 05:25:24 +0200

    edit cart class

--- Classes/Cart.java
+++ Classes/Cart.java
@@ -2,39 +2,63 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Cart {
-    private Map<Item, Integer> itemsAndAmounts;
+    private Map<Item, Integer> itemsAmounts;
     private double totalPrice;
 
     public Cart() {
-        this.itemsAndAmounts = new HashMap<>();
+        this.itemsAmounts = new HashMap<>();
         this.totalPrice = 0.0;
     }
 
     public void addToCart(Item item, int amount) {
         if (amount <= 0)
             throw new IllegalArgumentException("Amount must be positive.");
-        int currentAmount = itemsAndAmounts.getOrDefault(item, 0);
-        itemsAndAmounts.put(item, currentAmount + amount);
+        int currentAmount = itemsAmounts.getOrDefault(item, 0);
+        itemsAmounts.put(item, currentAmount + amount);
         item.ordered(amount);
         setTotalPrice();
     }
 
     public void removeFromCart(Item item) {
-        if (!itemsAndAmounts.containsKey(item))
+        if (!itemsAmounts.containsKey(item))
             throw new IllegalArgumentException("Item not found in cart.");
-        itemsAndAmounts.remove(item);
+        itemsAmounts.remove(item);
         item.unordered();
         setTotalPrice();
     }
 
+    public void increaseAmount(Item item) {
+        if (!itemsAmounts.containsKey(item))
+            throw new IllegalArgumentException("Item not found in cart.");
+
+        int currentAmount = itemsAmounts.get(item);
+        itemsAmounts.put(item, currentAmount + 1);
+        setTotalPrice();
+    }
+
+    public void decreaseAmount(Item item) {
+        if (!itemsAmounts.containsKey(item))
+            throw new IllegalArgumentException("Item not found in cart.");
+
+        int currentAmount = itemsAmounts.get(item);
+        if (currentAmount == 1)
+            removeFromCart(item);
+        else {
+            itemsAmounts.put(item, currentAmount - 1);
+            item.unordered();
+        }
+        setTotalPrice();
+    }
+
+
     public void emptyCart() {
-        itemsAndAmounts.clear();
+        itemsAmounts.clear();
         setTotalPrice();
     }
 
     private void setTotalPrice() {
         double price = 0;
-        for (Map.Entry<Item, Integer> entry : itemsAndAmounts.entrySet())
+        for (Map.Entry<Item, Integer> entry : itemsAmounts.entrySet())
             price += entry.getKey().getPrice() * entry.getValue();
         totalPrice = price;
     }
