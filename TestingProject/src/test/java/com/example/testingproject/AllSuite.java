e821eb7c49f479b982172470e46cb02a26d53b27
Mariam-Abdou <126914458+Mariam-Abdou@users.noreply.github.com>
2024-04-26 02:51:18 +0300

    test suite

--- TestingProject/src/test/java/com/example/testingproject/AllSuite.java
+++ TestingProject/src/test/java/com/example/testingproject/AllSuite.java
@@ -4,17 +4,16 @@ import org.junit.platform.suite.api.Suite;
 
 @Suite
 @SelectClasses({
-	 //  CartTest.class,
-    // CatalogTest.class,
-     EcomSystemTest.class,
-   //  ItemTest.class,
-    OrderTest2.class,
-     UserTest2.class,
-     VisaTest.class
-    // Add more test classes here as needed
+        CartTest.class,
+        CatalogTest.class,
+        EcomSystemTest.class,
+        ItemTest.class,
+        // OrderTest2.class,
+        UserTest2.class,
+        VisaTest.class
 })
 
-  
+
 public class AllSuite {
 
 }
