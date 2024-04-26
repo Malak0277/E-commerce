ee9576df3cc702c6bb043e82c1cb69c3c8e61102
Siveen3 <127445863+Siveen3@users.noreply.github.com>
2024-04-26 15:10:13 +0300

    Update ItemTest.java

--- TestingProject/src/test/java/com/example/testingproject/ItemTest.java
+++ TestingProject/src/test/java/com/example/testingproject/ItemTest.java
@@ -6,7 +6,15 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ItemTest {
     private Item item;
+    @BeforeAll
+    static void setUpBeforeClass() throws Exception {
+        System.out.println("Initiating Item Testing");
+    }
 
+    @AfterAll
+    static void tearDownAfterClass() throws Exception {
+        System.out.println("Done Item Testing");
+    }
     @BeforeEach
     void setUp() {
         item = new Item("The Da Vinci Code", "Mystery", 11.25, 18, "da_vinci_code.jpg");
@@ -21,6 +29,7 @@ class ItemTest {
     @DisplayName("Tests for order and unorder methods")
     class OrderedTests {
         @Test
+        @Order(1)
         @DisplayName("Test for order method with  valid amount")
         void ordered_valid() {
             item.ordered(10);
@@ -28,6 +37,7 @@ class ItemTest {
         }
 
         @Test
+        @Order(2)
         @DisplayName("Test for unorder method ")
         void unordered(){
             int intialStock = item.getStock();
@@ -43,21 +53,25 @@ class ItemTest {
     @DisplayName("tests for getters ")
     class GettersTests {
         @Test
+        @Order(3)
         void getName() {
             assertEquals("The Da Vinci Code", item.getName());
         }
 
         @Test
+        @Order(4)
         void getPrice() {
             assertEquals(11.25, item.getPrice());
         }
 
         @Test
+        @Order(5)
         void getStock() {
             assertEquals(18, item.getStock());
         }
 
         @Test
+        @Order(6)
         void getImage() {
             assertEquals("/images/Books/da_vinci_code.jpg", item.getImage());
         }
